ce0ca8266fcf416dbc8396225c4ca6b3d68d0050
Kulakarni Balaji <vk9677022@gmail.com>
2023-12-09 22:34:47 +0530

    Files added

--- booking-service/src/main/java/com/movie/ticket/booking/system/service/kafka/publisher/BookingServiceKafkaPublisher.java
+++ booking-service/src/main/java/com/movie/ticket/booking/system/service/kafka/publisher/BookingServiceKafkaPublisher.java
@@ -1,9 +1,6 @@
 package com.movie.ticket.booking.system.service.kafka.publisher;
-
-
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import com.movie.ticket.booking.system.commons.dto.BookingDTO;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -19,8 +16,6 @@ public class BookingServiceKafkaPublisher {
 
     public void publishToPaymentRequestTopic(BookingDTO bookingDTO) {
         log.info("publishing booking details to the payment-request kafka topic");
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JavaTimeModule());
         try {
             this.kafkaTemplate.send("payment-request", objectMapper.writeValueAsString(bookingDTO));
         } catch (JsonProcessingException e) {
